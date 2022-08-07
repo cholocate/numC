0eee9cdcacfb4c2de5b8d3c6aa3d57291c1a9844
Mehul Gandhi <gandhi@berkeley.edu>
2022-08-07 12:55:52 -0700

    transpose

--- src/matrix.c
+++ src/matrix.c
@@ -353,7 +353,7 @@ int transpose_matrix(matrix *result, matrix *mat2) {
     int index = 0;
     for (int col = 0; col < colL; col++) {
         for (int row = 0; row < rowL; row++) {
-            result[index] = mat2[row * colL + col]; //column major order
+            result[index] = mat2[col + row * colL]; //column major order
             index++;
         }
     }
