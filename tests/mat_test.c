58f59dbdaad81231ef4206359f2fe5a6da96ac5e
Mehul Gandhi <gandhi@berkeley.edu>
2022-08-07 12:25:30 -0700

    .

--- tests/mat_test.c
+++ tests/mat_test.c
@@ -342,7 +342,7 @@ int main (void)
         (CU_add_test(pSuite, "dealloc_null_test", dealloc_null_test) == NULL) ||
         (CU_add_test(pSuite, "get_test", get_test) == NULL) ||
         (CU_add_test(pSuite, "set_test", set_test) == NULL) ||
-        (CU_add_test(pSuite, "sub_test", sub_test) == NULL)
+        (CU_add_test(pSuite, "trasnpose_matrix", transpose_test) == NULL)        
      )
    {
       CU_cleanup_registry();
